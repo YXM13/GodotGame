89615297cb1dcc3e65c8233e692e6b6d25c18c82
markmal <markmal@kth.se>
2026-05-19 12:16:14 +0200

    player sprite flips depending on the direction they are moving

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 const SPEED = 500.0
-const ACCELERATION = 3000.0
-const FRICTION = 6000.0
+const ACCELERATION = 5000.0
+const FRICTION = 8000.0
 
 const GRAVITY = 2000.0
 const JUMP_FORCE = -700.0
@@ -14,6 +14,12 @@ var spawn_position = Vector2(7830, -50)
 var lives 
 var collectedCoins
 var time
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _ready():
+	global_position = spawn_position
+	lives = 1
+	collectedCoins = 0
 
 func _physics_process(delta):
 	var input_dir = Input.get_axis("ui_left", "ui_right")
@@ -27,7 +33,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_FORCE
 		
-# Variable jump height
+	# Variable jump height
 	if Input.is_action_just_released("ui_accept") and velocity.y < 0:
 		velocity.y *= 0.5
 
@@ -37,7 +43,12 @@ func _physics_process(delta):
 		if not is_on_floor():
 			accel *= AIR_CONTROL
 		
-		velocity.x = move_toward(velocity.x, input_dir * SPEED, accel * delta)
+		var target_speed = input_dir * SPEED
+
+		if sign(velocity.x) != sign(target_speed) and abs(velocity.x) > 10:
+			accel *= 2.5
+
+		velocity.x = move_toward(velocity.x, target_speed, accel * delta)
 	else:
 		var fric = FRICTION
 		if not is_on_floor():
@@ -45,9 +56,10 @@ func _physics_process(delta):
 		
 		velocity.x = move_toward(velocity.x, 0, fric * delta)
 
-	move_and_slide()
+	# Direction
+	if input_dir != 0:
+		animated_sprite.flip_h = input_dir < 0
+		animated_sprite.play("walk")
+	
 
-func _ready():
-	global_position = spawn_position
-	lives = 1
-	collectedCoins = 0
+	move_and_slide()
