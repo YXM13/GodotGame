165dd0712652173b4f8bb54336850ff2ebb71d46
Mark Malki <markmal@ug.kth.se>
2026-05-20 23:27:32 +0200

    changes arent pushed correctly

--- scripts/goal_lvl_3.gd
+++ scripts/goal_lvl_3.gd
@@ -1,20 +1,20 @@
 extends Area2D
 
-
-
-
 func _on_body_entered(body: Node2D) -> void:
-	if(body.collectedCoins == 1):
-		
-		
+	if "collectedCoins" in body and body.collectedCoins == 1:
+
 		print("Wow, you actually beat the game... I'm impressed!")
+
 		%GameManager.thirdLevelTime = %GameManager.time - %GameManager.firstLevelTime - %GameManager.secondLevelTime
 		print("Time to beat third level: ", %GameManager.format_time(%GameManager.thirdLevelTime))
-		
-		
+
 		%GameManager.stop_timer()
-		
-		get_tree().change_scene_to_file("res://scenes/gameover.tscn")
-		
+
+		call_deferred("go_to_gameover")
+
 	else:
 		print("You need the coin first!")
+
+func go_to_gameover():
+	get_tree().change_scene_to_file("res://scenes/gameover.tscn")
+	
