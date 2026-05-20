71184126378fe14541fd8b9fc3494f6c5776f198
Mark Malki <markmal@ug.kth.se>
2026-05-20 21:28:29 +0200

    added start and end screen huds

---
+++ gameover.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _on_play_again_pressed():
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_quit_pressed():
+	get_tree().quit()
