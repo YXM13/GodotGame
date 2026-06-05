464630e02089eff34f0d722e3f9224197d2a8355
YXM13 <fabiantq13@gmail.com>
2026-06-05 18:35:31 +0200

    Added enemy movement that makes the enemy constantly walks towards the player

--- scripts/hurtzone.gd
+++ scripts/hurtzone.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
+	var player = get_tree().get_first_node_in_group("player")
 	print("You took damage!")
 	body.lives -= 1
 	if(body.lives == 0):
